410c5917dbc7432a13ccab1e137732dca25ff762
csonal0947 <csonal0947@gmail.com>
2026-03-22 01:21:08 +0530

    Add stock data population to build script

--- backend-drf/build.sh
+++ backend-drf/build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Populate stock data if database is empty
+python manage.py add_sample_stocks
+python manage.py fetch_exchange_listings || true
