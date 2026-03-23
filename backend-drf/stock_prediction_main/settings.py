3fac17d3223ba6abc0ebcc130440b14c3a2107f8
csonal0947 <csonal0947@gmail.com>
2026-03-23 17:12:22 +0530

    Update project files

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -134,6 +134,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5175',
     'http://localhost:5176',
     'https://stock-lens-s.netlify.app',
+    'https://stock-lens-six.vercel.app',
 ]
 CORS_ALLOW_HEADERS = ['*']
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
